788e425072ecf91ed74a6ca91a942e3f037797d9
joernschubert-ai <joern.schubert@gmail.com>
2026-03-28 11:49:52 +0100

    Add files via upload

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -6,7 +6,7 @@ const { password } = await req.json();
 console.log("INPUT:", password);
 console.log("ENV:", process.env.APP_PASSWORD);
 
-if (password === process.env.APP_PASSWORD) {
+if (password?.trim() === process.env.APP_PASSWORD?.trim()) {
 return NextResponse.json({
 success: true,
 token: "my-secret-token-123"
