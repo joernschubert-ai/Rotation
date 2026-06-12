6cb92a64136721ee4a9613fa68f6b65301ddf774
joernschubert-ai <joern.schubert@gmail.com>
2026-06-12 13:01:10 +0200

    Add files via upload

--- app/api/market/route.ts
+++ app/api/market/route.ts
@@ -2448,13 +2448,13 @@ export async function GET(req: Request) {
 
 console.log("AUTH HEADER RAW:", req.headers.get("authorization"));
 
-const token = req.headers.get("authorization");
+// const token = req.headers.get("authorization");
 
-console.log("TOKEN AFTER GET:", token);
+// console.log("TOKEN AFTER GET:", token);
 
-if (!token || !token.includes("x9KfP2LmQa83zZ_2519.BJ")) {
-return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-}
+// if (!token || !token.includes("x9KfP2LmQa83zZ_2519.BJ")) {
+// return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+// }
 const now = Date.now();
 
 if (
