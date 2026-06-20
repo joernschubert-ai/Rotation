fbe1bfabf70236cd3db72a279bc2a2a518d25ec5
joernschubert-ai <joern.schubert@gmail.com>
2026-06-20 17:10:35 +0200

    Add files via upload

--- lib/engine/marketEngine.ts
+++ lib/engine/marketEngine.ts
@@ -1115,18 +1115,6 @@ historicalReplay(replaySnapshots);
 RETURN
 ===================================================== */
 
-console.log("MARKET PHASE RAW:", phase);
-console.log(
-"PHASE FOR SIGNAL:",
-phase
-);
-
-console.log(
-"PHASE OBJECT:",
-marketPhase
-);
-
-
 return {
 
 crash,
